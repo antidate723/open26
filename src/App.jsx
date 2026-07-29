f295f06f893a25f6b968b9d8da4914f4b660fa70
antidate723 <lapinfoo@gmail.com>
2026-07-29 23:51:23 +0300

    modfic putin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import StartScreen from "./components/StartScreen";
+//import DarkRoom from "./components/DarkRoom.jsx";
+import "./App.css";
 
+const STORAGE_KEY = "escapeRoomProgress";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [started, setStarted] = useState(false);
+  const [solved, setSolved] = useState([false, false, false]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem(STORAGE_KEY);
+    if (saved) {
+      const data = JSON.parse(saved);
+      setStarted(data.started);
+      setSolved(data.solved);
+    }
+  }, []);
+
+  useEffect(() => {
+    const data = { started, solved };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  }, [started, solved]);
 
   return (
     <>
+      {!started ? (
+        <StartScreen onStart={() => setStarted(true)} />
+      ) : (
+        <DarkRoom solved={solved} setSolved={setSolved} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
